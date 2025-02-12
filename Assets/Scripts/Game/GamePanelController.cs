9591140abde6c6fc9c6703b56e2afdcd8aea1cb1
Junil Ko <goyadb@outlook.com>
2025-02-12 13:40:47 +0900

    Object Pool 추가

--- Assets/Scripts/Game/GamePanelController.cs
+++ Assets/Scripts/Game/GamePanelController.cs
@@ -1,10 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GamePanelController : MonoBehaviour
 {
+    [SerializeField] private GameObject quizCardPrefab;                        // Quiz Card Prefab
+    [SerializeField] private Transform quizCardParent;                         // Quiz Card가 표시될 UI Parent
+    
+    private void Start()
+    {
+        ShowQuizCard();
+    }
 
+    private void ShowQuizCard()
+    {
+        var quizCardObject = Instantiate(quizCardPrefab, quizCardParent);
+    }
+    
     public void OnClickGameOverButton()
     {
         GameManager.Instance.QuitGame();
