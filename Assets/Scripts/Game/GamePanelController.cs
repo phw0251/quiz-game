34c77f5d3fa73bad2e613de03f0782e281acdf12
Junil Ko <goyadb@outlook.com>
2025-02-12 10:51:08 +0900

    메인씬 구성

---
+++ Assets/Scripts/Game/GamePanelController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePanelController : MonoBehaviour
+{
+
+    public void OnClickGameOverButton()
+    {
+        
+    }
+}
